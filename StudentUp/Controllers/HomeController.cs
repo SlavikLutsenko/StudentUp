716704c1d23837fbd584e282fea32947b2e5f02d
Slavik <slavka.com.ua@gmail.com>
2014-06-02 18:42:36 +0300

    v0.0007 Добавленна авторизация Добавленна мастер страница для зарегистрирова пользователей

--- StudentUp/Controllers/HomeController.cs
+++ StudentUp/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using StudentUp.Models;
 
 namespace StudentUp.Controllers
 {
@@ -16,5 +13,25 @@ namespace StudentUp.Controllers
             return View();
         }
 
+		/// <summary>
+		/// Производит авторизацию пользователей
+		/// </summary>
+		/// <param name="email">Email пользователя</param>
+		/// <param name="password">Пароль пользователя</param>
+		/// <returns>Если пользователь зарегистрирован и ввел правильные данные то он авторизуется иначе его перенаправят на главную страницу с соответствуещим сообщением. </returns>
+		[HttpPost]
+		public ActionResult Index(string email, string password)
+		{
+			Users user = new Users(email, password);
+			if (!user.IsLogin()) return View();
+			user.GetInformationAboutUserFromDB();
+			user.CreateSession(Response);
+			return Redirect("/home");
+		}
+
+	    public ActionResult Home()
+	    {
+		    return View();
+	    }
     }
 }
